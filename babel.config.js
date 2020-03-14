437f17e82656946d33f01e608809cd99317f1510
viricruz <viridiana.cruz.olivera@gmail.com>
2020-03-13 19:35:24 -0700

    Add babel configuration file

---
+++ babel.config.js
@@ -0,0 +1,35 @@
+// module.exports = {
+//   presets: [
+//     [
+//       '@babel/preset-env',
+//       {
+//         targets: {
+//           browsers: [
+//             ">0.25%",
+//             "not ie 11",
+//             "not op_mini all"
+//           ],
+//         },
+//         modules: false
+//       },
+//     ],
+//   ],
+// };
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+          browsers: [
+            ">0.25%",
+            "not ie 11",
+            "not op_mini all"
+          ]
+        },
+      },
+    ],
+  ],
+};
