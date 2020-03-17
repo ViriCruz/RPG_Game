327974a842563a75b0b0cbdc5da541f44589cb20
viricruz <viridiana.cruz.olivera@gmail.com>
2020-03-17 12:33:58 -0700

    Add top10 players

--- src/scenes/LeaderboardScene.js
+++ src/scenes/LeaderboardScene.js
@@ -8,26 +8,34 @@ export default class LeaderboardScene extends Phaser.Scene {
     super({ key: 'Leaderboard' });
   }
 
-  create() {
+  async create() {
     this.title = this.add.text(80, 20, 'Leaderboard', { fontSize: '12px', fill: '#fff', fontFamily: 'Arial' });
     this.leaderboard = new Leaderboard();
     this.score = new Score();
     this.score.checkScore();
-    const userScore = this.score.getScore();
-    const user = this.score.getUser();
-    this.leaderboard.postScore(user, userScore);
+    const points = this.score.getScore();
+    const username = this.score.getUser();
+    this.userScore = {
+      user: username,
+      score: points
+    }
 
-    this.leaderboard.getScores()
-      .then((response) => {
-        setTimeout(() => {
-          let { y } = this.title;
-          response.result.forEach((score) => {
-            y += 20;
-            this.add.text(80, y, score.user, { fontSize: '12px', fill: '#fff', fontFamily: 'Arial' });
-            this.add.text(160, y, score.score, { fontSize: '12px', fill: '#fff', fontFamily: 'Arial' });
-          });
-        }, 2000);
-      })
-      .catch((error) => error);
+    const post = await this.leaderboard.postScore(this.userScore)
+    const data = await this.leaderboard.loadScores()
+
+    this.best10 = data.result.sort((score1, score2) => score1.score < score2.score ? 1 : -1)
+    .filter((score, i) => i < 10)
+    
+    this.displayBestPlayers()
+   
+  }
+
+  displayBestPlayers() {
+    let { y } = this.title;
+    this.best10.forEach(player => {
+      y += 20;
+      this.add.text(80, y, player.user, { fontSize: '12px', fill: '#fff', fontFamily: 'Arial' });
+      this.add.text(160, y, player.score, { fontSize: '12px', fill: '#fff', fontFamily: 'Arial' });
+    });
   }
 }
