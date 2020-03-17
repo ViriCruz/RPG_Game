a9701dd34e54661a1caa1a3b9944c41d71899f92
viricruz <viridiana.cruz.olivera@gmail.com>
2020-03-17 12:32:19 -0700

    Add module regenerator runtime

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Phaser from 'phaser';
+import 'regenerator-runtime';
 import BootScene from './scenes/BootScene';
 import WorldScene from './scenes/WorldScene';
 import BattleScene from './scenes/BattleScene';
 import UIScene from './scenes/UIScene';
 import LeaderboardScene from './scenes/LeaderboardScene';
+import Leaderboard from './modules/Leaderboard'
 import Dom from './modules/Dom';
 
 
@@ -46,5 +48,38 @@ Dom().button.addEventListener('click', (e) => {
   }
   e.preventDefault();
 });
+
+
+const l = new Leaderboard()
+
+// const postData = async () => {
+//   const score = {
+//     user: 'user0001',
+//     score: 10000
+//   }
+//   const data = await l.postScore(score)
+//   console.log(data)
+// }
+
+// const d = async () => {
+//   const data = await l.loadScores()
+//   console.log(data);
+// }
+// postData()
+// d()
+
+// const resolveData = async() => {
+//   const post = await l.postScore({
+//     user: 'user007',
+//     score: 1000
+//   })
+//   const scores = await l.loadScores()
+//   return [post, scores]
+// }
+
+// console.log(resolveData());
+// Promise.all(resolveData).then( values => {
+//   console.log(values);
+// })
 
 export default game;
